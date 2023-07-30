6d7eae5b4171d6d43b2fd8467a485818b1a6677d
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-07-30 11:17:20 -0400

    added category columns

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,18 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    //defined columns to be id and categoryName
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
